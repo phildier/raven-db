692189489674d455ec9a28f6d5d009469a73735d
Phil Dier <phil@raventools.com>
2016-03-31 17:09:40 -0500

    download to cache dir

--- recipes/install_dynamodb.rb
+++ recipes/install_dynamodb.rb
@@ -11,14 +11,14 @@ directory dynamodb_datadir do
 	group "nobody"
 end
 
-remote_file "#{dynamodb_dir}/dynamodb.tar.gz" do
+remote_file "#{node[:raven_deploy][:attachments_dir]}/dynamodb.tar.gz" do
 	source "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz"
 end
 
 bash "extract dynamodb" do
 	cwd dynamodb_dir
 	code <<-EOH
-	tar xf dynamodb.tar.gz
+	tar xf "#{node[:raven_deploy][:attachments_dir]}/dynamodb.tar.gz"
 	EOH
 	not_if { ::File.exists?("#{dynamodb_dir}/DynamoDBLocal.jar") }
 end
