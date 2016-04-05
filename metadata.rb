fa1faf9c38ebd2cbad9166385c428967eea66579
Byron Albert <byron@raventools.com>
2016-04-05 11:04:15 -0500

    add new memcached recipe

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,50 @@ version          '0.1.1'
 depends "raven-deploy"
 depends "raven-supervisor"
 depends "yum-mysql-community"
+
+recipe "raven-db::memcached", "install and configure memcached"
+
+attribute "raven_db",
+	:display_name => "Raven DB",
+	:type => "hash"
+
+attribute "raven_db/memcached_port",
+	:display_name => "Memcached port",
+	:description => "Memcache port",
+	:required => "recommended",
+	:type => "string",
+	:recipes => ["raven-db::memcached"],
+	:default => "11211"
+
+attribute "raven_db/memcached_user",
+	:display_name => "Memcached user",
+	:description => "Memcached user",
+	:required => "recommended",
+	:type => "string",
+	:recipes => ["raven-db::memcached"],
+	:default => "memcached"
+
+attribute "raven_db/memcached_max_conn",
+	:display_name => "max connections for memcached",
+	:description => "max connections for memcached",
+	:required => "recommended",
+	:type => "string",
+	:recipes => ["raven-db::memcached"],
+	:default => "4096"
+
+attribute "raven_db/memcached_max_mem",
+	:display_name => "max memory in mb memcached",
+	:description => "max memory in mb memcached",
+	:required => "recommended",
+	:type => "string",
+	:recipes => ["raven-db::memcached"],
+	:default => "900"
+
+attribute "raven_db/memcached_socket",
+	:display_name => "memcached socket path",
+	:description => "memcached socket path",
+	:required => "recommended",
+	:type => "string",
+	:recipes => ["raven-db::memcached"],
+	:default => ""
+
